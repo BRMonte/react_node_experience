8caba4ea458a2d35f6d0852c13445a8571e3b1b8
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-17 23:16:39 +0000

    Bridhe the API and the front-end

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
+const path = require("path");
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
+//allow node to access our built React project
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+//handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server" });
+});
+
+//all other GET requests not handle before will return the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
